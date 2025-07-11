23c3e4189f77e89a931a57a68b5f7027d47854f1
amit-i8cloud <agangwar@i8cloudconsulting.com>
2025-07-11 14:51:38 +0530

    Learnt the Single Level Inheritance,how to extend its functionaity using the super()

---
+++ Oops/p4.py
@@ -0,0 +1,85 @@
+'''
+Inheritance :
+-> ITs when a child class inherits the properties and methods of the parent class
+-> It allows us to reuse the code from another class
+-> Code Reusability
+-> Less Deuplication
+-> A child class can inherit and access everything from the parent class except private members(__name)
+-> Organized due to fact that different features have differnt classes so by searching the class name we can go the that class.
+'''
+
+# Without Inheritance
+# class Parent:
+#     def method1(self):
+#         print("Parent Class !!")
+#
+# class Child:
+#     def method2(self):
+#         print("Child Class !!")
+# :: Child class now have access to all public and protected members of the parent class .
+# We will discuss them later one by one .
+
+# obj = Child()
+# obj.method2()
+# obj.method1() # cant access as till now we had not inherited the parent class .
+
+# With Inheritance
+# class Parent:
+#     def method1(self):
+#         print("Parent Class !!")
+#
+# class Child(Parent):
+#     def method2(self):
+#         print("Child Class !!")
+#
+# obj = Child()
+# obj.method2()
+# obj.method1() # cant access as till now we had not inherited the parent class .
+
+'''              Types of Inheritance        '''
+'''
+Single Inheritance :: 
+-> One child class inherits from one parent class
+'''
+class i8Cloud:
+    def employee(self):
+        print("Employee from i8Cloud !!")
+
+
+class Meta(i8Cloud):
+
+    # below is Overwriting the existing the parent child content
+    # def employee(self):
+    #     print("Employee from Facebook")
+
+    # below is the extending functionality of the Parent class
+    # def employee(self):
+    #     super().employee()
+    #     print("Its a Good Employee !!")
+
+    def employee2(self):
+       # super().employee()   # calling the function of the parent class using super
+        self.employee()  # calling the function of the parent class
+        print("Employee from Meta !!")
+
+obj = Meta()
+# print(obj.employee())
+print(obj.employee2())
+'''
+Multilevel Inheritance :: 
+-> In Multilevel Inheritance, a class inherits from a child class, which itself inherited from another class.
+'''
+'''
+Hierarchical  Inheritance :: 
+-> In Hierarchical Inheritance, multiple child classes inherit from the same parent class.
+'''
+
+'''
+Multiple Inheritance :: 
+-> A single child class inherits from more than one parent class.
+'''
+'''
+Single Inheritance :: 
+-> One child class inherits from one parent class
+'''
+
