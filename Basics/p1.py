dc9a5833a629aca76b78c5f29870a9b6b4760d06
amit-i8cloud <agangwar@i8cloudconsulting.com>
2025-07-15 15:04:28 +0530

    Covered some of small topics

---
+++ Basics/p1.py
@@ -0,0 +1,59 @@
+# Python is a Programming Language created by guido van rossum
+"""
+What can Python do ?
+ -> Can be used as a backened to create servers.
+ -> it can read and modify files , used to connect to db
+ -> used to handle big data and perform complex maths clculations
+ ->
+"""
+# x = 4.5
+# print(type(x))
+#
+# # casting -> specifying the data type of a variable explitcilly
+# y = float(3)
+# print(y)
+# print(type(y))
+#
+# #Camelcase
+# myVariableName = 'John'
+#
+# #Pascelcase
+# MyVariableName ="John"
+#
+# #snake_case
+# my_variable_name = "John"
+#
+# # Many values to multiple variables
+# x,y,z = "Orange, ","Banana, ","Cherry"
+# print(x,y,z)
+#
+# # one value to multiple variables
+# a = b = c = "Hello Dude, "
+# print(a,b,c)
+#
+# # unpacking
+# fruits =["apple","banana","cherry"]
+# x1,y1,z1 = fruits
+# print(x1,y1,z1)
+
+# x = 5
+# y = "Hello"
+# print(x + y)  # error
+x = "awesome"
+
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x) # Python is fantastic
+#
+# myfunc()
+#
+# print("Python is " + x) # Python is awesome
+
+# x = "awesome"
+# def myFun():
+#     global x  # it tells that x inside this scope treat as global
+#     x = "fantastic"
+#     print(x)  # fantastic
+#
+# myFun()
+# print("Python is "+ x)  # Python is awesome
