803742eee50a32abc89eb6cd2dfb56d2059bd5ff
amit-i8cloud <agangwar@i8cloudconsulting.com>
2025-07-15 15:32:15 +0530

    learnt built in data types

---
+++ Basics/p2.py
@@ -0,0 +1,22 @@
+# Built in Data types
+"""
+str
+int,float,complex
+list,tuple,range
+bool
+dict
+set
+bytes
+"""
+import random
+
+# type conversion
+# x = '4'
+# print(type(x))
+#
+# a = float(x)
+# b = complex(x)
+# print(type(a),type(b))
+
+# below prints random number from the specified range
+print(random.randrange(1,9))
